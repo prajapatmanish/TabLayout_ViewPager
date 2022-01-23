4b8aa3964198b184a4515e3da4c311bef751e89b
manish prajapat <manishprajapat492@gmail.com>
2022-01-23 16:27:02 +0530

    first commit

---
+++ app/src/main/java/com/project/manishprajapat/tabactivity/CallsFragment.java
@@ -0,0 +1,22 @@
+package com.project.manishprajapat.tabactivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CallsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_calls, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/project/manishprajapat/tabactivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.project.manishprajapat.tabactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager=findViewById(R.id.view_pager);
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+
+        tabLayout=findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
